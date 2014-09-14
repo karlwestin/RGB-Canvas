bd2119b055a28b25dce9f72ed6ae4d2c16797e10
Karl Westin <karl.westin@gmail.com>
2014-09-14 13:19:12 +0200

    Fix 'image' element => img Take a callback for when data's been loaded

--- rgbcanvas.js
+++ rgbcanvas.js
@@ -1,76 +1,83 @@
-var RGBCanvas = function(imgEl, el) {
+var RGBCanvas = function(imgEl, el, cb) {
+  if(typeof el == "function") {
+    cb = el;
+    el = null;
+  }
+  el || (el = document.body);
 
-      el || (el = document.body);
-      if (typeof imgEl === "string")
-          loadImage(imgEl, startShit);
-      else
-          startShit(imgEl);
-      
-      var canvas = document.createElement("canvas"),
-          ctx = canvas.getContext("2d"),
-          pixels = [],
-          img;
+  if (typeof imgEl === "string")
+      loadImage(imgEl, startShit);
+  else
+      startShit(imgEl);
 
-      el.appendChild(canvas);
+  var canvas = document.createElement("canvas"),
+      ctx = canvas.getContext("2d"),
+      pixels = [],
+      img;
 
-      function loadImage(url, callback) {
-          img = document.createElement("image");
-          img.src = url;
-          img.onload = function() { callback(this) }; 
-      }
+  el.appendChild(canvas);
 
-      function startShit(imgEl) {
-        img || (img = imgEl);
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-      }
+  function loadImage(url, callback) {
+      img = document.createElement("img");
+      img.src = url;
+      img.onload = function() { callback(this); };
+  }
 
-      function readPixels (image) {
-         return ctx.getImageData(0, 0, img.width, img.height).data;
-      }
+  function startShit(imgEl) {
+    img || (img = imgEl);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    if(cb) {
+      cb();
+    }
+  }
+
+  function readPixels (image) {
+      return ctx.getImageData(0, 0, img.width, img.height).data;
+  }
 
   return {
-      
+
   ctx: ctx,
 
   readPixels: readPixels,
-    
+
   transformRGB: function(config) {
-        config = {
-          r:        config.r || false,
-          rOffset:  config.rOffset || 0,
-          g:        config.g || false,
-          gOffset:  config.gOffset || 0,
-          b:        config.b || false,
-          bOffset:  config.bOffset || 0,
-          data:     config.data
-        }
+    config = {
+      r:        config.r || false,
+      rOffset:  config.rOffset || 0,
+      g:        config.g || false,
+      gOffset:  config.gOffset || 0,
+      b:        config.b || false,
+      bOffset:  config.bOffset || 0,
+      data:     config.data
+    };
 
-        
-        var newData = ctx.createImageData(canvas.width, canvas.height);
-        newData.data = config.data;
-        
-        var x = 0;
-        for(var i = 0; i < config.data.length; i+=4) {
-          newData.data[i] = (config.r) ?
-            config.r(config.data[i + 4*config.rOffset])   :
-            config.data[i];
-             
-          newData.data[i+1] = (config.g) ?
-            config.g(config.data[i+1 + 4*config.gOffset]) :
-            config.data[i+1];
 
-          newData.data[i+2] = (config.b) ?
-            config.b(config.data[i+2 + 4*config.bOffset]) :
-            config.data[i+2];
+    var newData = ctx.createImageData(canvas.width, canvas.height);
+    newData.data = config.data;
 
-          newData.data[i+3]     = 255; // Alpha
-        }
-        return newData;   
+    var x = 0;
+    for(var i = 0; i < config.data.length; i+=4) {
+      newData.data[i] = (config.r) ?
+        config.r(config.data[i + 4*config.rOffset])   :
+        config.data[i];
+
+      newData.data[i+1] = (config.g) ?
+        config.g(config.data[i+1 + 4*config.gOffset]) :
+        config.data[i+1];
+
+      newData.data[i+2] = (config.b) ?
+        config.b(config.data[i+2 + 4*config.bOffset]) :
+        config.data[i+2];
+
+      newData.data[i+3]     = 255; // Alpha
+    }
+    return newData;
   },
 
   };
-  
+
 };
 
